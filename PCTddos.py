1ad325141650f5aaee7ea90084135e518fb6a749
Congching <114208687+Congching@users.noreply.github.com>
2023-01-12 19:05:23 +0700

    Update PCTddos.py

--- PCTddos.py
+++ PCTddos.py
@@ -1,5 +1,5 @@
 ##############################
-# ZxCDDoS Made by PCT   #
+# PCT Made by PCT   #
 # COPY = NIGGER              #
 ##############################
 
@@ -229,7 +229,7 @@ def menu():
 def main():
     menu()
     while(True):
-        cnc = input('''\x1b[38;2;0;212;14m╔══[PCTddos•@phamcongtrinh\x1b[38;2;0;186;45m@d\x1b[38;2;0;150;88mx\x1b[38;2;0;113;133mC\x1b[38;2;0;49;147m]
+        cnc = input('''\x1b[38;2;0;212;14m╔══[PCTddos@root@phamcongtrinh\x1b[38;2;0;186;45m@d\x1b[38;2;0;150;88mx\x1b[38;2;0;113;133mC\x1b[38;2;0;49;147m]
 \x1b[38;2;0;212;14m╚\x1b[38;2;0;186;45m═\x1b[38;2;0;150;88m═\x1b[38;2;0;113;133m═\x1b[38;2;0;83;168m═\x1b[38;2;0;49;147m➤ \x1b[38;2;239;239;239m''')
         if cnc == "layer7" or cnc == "LAYER7" or cnc == "L7" or cnc == "l7":
             layer7()
